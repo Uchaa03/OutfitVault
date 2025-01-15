ec55e813d202ddc23bcb8913800f060d3cfaeca0
uchaa03 <uchasousaa@gmail.com>
2025-01-15 13:32:54 +0100

    Forms Structure Updated

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,14 +4,22 @@ import FormLayout from "../layouts/FormLayout.jsx";
 const LoginPage = () => {
   return (
       <FormLayout>
-          <h1 className="section__title">Inicia Sesión</h1>
+          <header className="section__header">
+              <h1 className="header__title">Inicia Sesión</h1>
+              <button className="header__button">Regístrate</button>
+          </header>
           <article className="section__article">
               <img className="article__img" src="/assets/img/IconUser.svg" alt="Imagen de incio de sesión"/>
               <form className="article__form">
-                  <label className="form__label">Nombre de Usuario</label>
-                  <input className="form__input" type="text"/>
-                  <label className="form__label">Contraseña</label>
-                  <input className="form__input" type="text"/>
+                  <section className="form__section">
+                      <label className="section__label">Nombre de Usuario</label>
+                      <input className="section__input" type="text"/>
+                  </section>
+                  <sectionf className="form__section">
+                      <label className="section__label">Contraseña</label>
+                      <input className="section__input" type="text"/>
+                  </sectionf>
+                  <button className="form__button">Iniciar Sesión</button>
               </form>
           </article>
 
