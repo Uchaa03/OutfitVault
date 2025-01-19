3e8b2584f087050af97559511587bcd9a84ca285
Adrián Ucha Sousa <uchasousaa@gmail.com>
2025-01-19 20:13:00 +0100

    Form Implemented with Validation.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,23 +1,72 @@
 import FormLayout from "../layouts/FormLayout.jsx";
+import {Formik} from "formik";
+import * as Yup from "yup";
+import {
+    passwordValidation, requestValidation,
+    usernameValidation
+} from "../validationSchemaHook.jsx";
+import {NavLink} from "react-router-dom";
+import React from "react";
 
 
 const LoginPage = () => {
-  return (
+
+    const validationSchema = Yup.object({ //Object for customHook
+        username: usernameValidation,
+        password: passwordValidation,
+    })
+
+
+    return (
       <FormLayout>
           <header className="section__header">
               <h1 className="header__title">Inicia Sesión</h1>
-              <button className="header__button">Regístrate</button>
+              <NavLink className="header__button" to="/register">Regístrate</NavLink>
           </header>
-          <form className="section__form">
-              <img className="form__img" src="/assets/img/IconUser.svg" alt="Imagen de incio de sesión"/>
-              <fieldset className="form__fieldset">
-                  <label className="fieldset__label">Nombre de Usuario</label>
-                  <input className="fieldset__input" type="text"/>
-                  <label className="fieldset__label">Contraseña</label>
-                  <input className="fieldset__input" type="text"/>
-              </fieldset>
-              <button className="form__button">Iniciar Sesión</button>
-          </form>
+          <Formik //For control and validation form
+              initialValues={{email: '', username: '',password: '', passwordVerification: '', }}
+              onSubmit={onsubmit} //Call register backend API
+              validationSchema={validationSchema}
+          >{
+              ({
+                   values,
+                   handleChange,
+                   handleSubmit,
+                   isSubmitting,
+                   handleBlur,
+                   errors,
+                   touched,
+               }) => (
+                  <form className="section__form" onSubmit={handleSubmit}>
+                      <img className="form__img" src="/assets/img/IconUser.svg" alt="Imagen de registro"/>
+                      <fieldset className="form__fieldset">
+                          <label className="fieldset__label" htmlFor="username">Nombre de Usuario</label>
+                          <input
+                              className="fieldset__input"
+                              type="text"
+                              name="username"
+                              placeholder="Crea tu nombre de Usuario"
+                              value={values.username}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.username && touched.username && (<p>{errors.username}</p>)}
+                          <label className="fieldset__label" htmlFor="password">Contraseña</label>
+                          <input
+                              className="fieldset__input"
+                              type="password"
+                              name="password"
+                              placeholder="Introduce tu Contraseña"
+                              value={values.password}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.password && touched.password && (<p>{errors.password}</p>)}
+                      </fieldset>
+                      <button type="submit" disabled={isSubmitting} className="form__button">Registrate</button>
+                  </form>
+              )
+          }</Formik>
       </FormLayout>
   )
 }
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,10 +1,96 @@
 import FormLayout from "../layouts/FormLayout.jsx";
+import {Formik} from "formik";
+import * as Yup from "yup";
+import {
+    emailValidation,
+    passwordValidation,
+    passwordVerficationValidation,
+    usernameValidation
+} from "../validationSchemaHook.jsx";
+import {NavLink} from "react-router-dom";
 
 
 const RegisterPage = () => {
+
+    const validationSchema = Yup.object({ //Object for customHook
+        email: emailValidation,
+        username: usernameValidation,
+        password: passwordValidation,
+        passwordVerification: passwordVerficationValidation,
+    })
+
   return (
       <FormLayout>
-
+          <header className="section__header">
+              <h1 className="header__title">Registrate</h1>
+              <NavLink className="header__button" to="/login">Accede</NavLink>
+          </header>
+          <Formik //For control and validation form
+              initialValues={{email: '', username: '',password: '', passwordVerification: '', }}
+              onSubmit={onsubmit} //Call register backend API
+              validationSchema={validationSchema}
+          >{
+              ({
+                  values,
+                  handleChange,
+                  handleSubmit,
+                  isSubmitting,
+                  handleBlur,
+                  errors,
+                  touched,
+              }) => (
+                  <form className="section__form" onSubmit={handleSubmit}>
+                      <img className="form__img" src="/assets/img/IconUser.svg" alt="Imagen de registro"/>
+                      <fieldset className="form__fieldset">
+                          <label className="fieldset__label" htmlFor="email">Correo Electrónico</label>
+                          <input
+                              className="fieldset__input"
+                              type="text"
+                              name="email"
+                              placeholder="Introduce tu Correo"
+                              value={values.email}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.email && touched.email && (<p>{errors.email}</p>)}
+                          <label className="fieldset__label" htmlFor="username">Nombre de Usuario</label>
+                          <input
+                              className="fieldset__input"
+                              type="text"
+                              name="username"
+                              placeholder="Crea tu nombre de Usuario"
+                              value={values.username}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.username && touched.username && (<p>{errors.username}</p>)}
+                          <label className="fieldset__label" htmlFor="password">Contraseña</label>
+                          <input
+                              className="fieldset__input"
+                              type="password"
+                              name="password"
+                              placeholder="Introduce tu Contraseña"
+                              value={values.password}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.password && touched.password && (<p>{errors.password}</p>)}
+                          <label className="fieldset__label" htmlFor="passwordVerification">Repite la contraseña</label>
+                          <input
+                              className="fieldset__input"
+                              type="password"
+                              name="passwordVerification"
+                              placeholder="Verifica la contraseña"
+                              value={values.passwordVerification}
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                          />
+                          {errors.passwordVerification && touched.passwordVerification && (<p>{errors.passwordVerification}</p>)}
+                      </fieldset>
+                      <button type="submit" disabled={isSubmitting} className="form__button">Registrate</button>
+                  </form>
+              )
+          }</Formik>
       </FormLayout>
   )
 }
