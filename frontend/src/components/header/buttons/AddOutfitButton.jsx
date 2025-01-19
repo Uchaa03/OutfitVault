43779afa717c32238f4ac41493b523d8f1709358
Llavesuke <llavesukeprogram@gmail.com>
2025-01-19 21:23:33 +0100

    Fixed header structure

--- frontend/src/components/header/buttons/AddOutfitButton.jsx
+++ frontend/src/components/header/buttons/AddOutfitButton.jsx
@@ -1,19 +1,13 @@
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const AddOutfitButton = () => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    console.log('Navigating to /upload');
-    navigate('/upload'); // Navigates to the /upload route
-  };
-
+ 
   return (
-    <section className="header__button" onClick={handleClick}>
+    <NavLink to="/upload" className="header__button">
       <img src="/assets/img/add_icon.svg" alt="Add Icon" className="button__icon" />
-      <h2>Agregar</h2>
-    </section>
+      <span>Agregar</span>
+    </NavLink>
   );
 };
 
--- frontend/src/components/header/buttons/LoginButton.jsx
+++ frontend/src/components/header/buttons/LoginButton.jsx
@@ -1,16 +1,14 @@
+import { NavLink } from "react-router-dom"
 
 
 
 const LoginButton = () => {
-  const handleClick = () => {
-    console.log('Add Outfit button clicked')
-    // TODO: Implement functionality to add an outfit to the vault
-  }
+
   return (
-      <section className={'header__button'} onClick={handleClick}>
+      <NavLink to="/login" className="header__button">
         <img src={'/assets/img/login_icon.svg'}  alt={'Login Icon'} className={'button__icon'}/>
-        <h2>Accede</h2>
-      </section>
+        <span>Accede</span>
+      </NavLink>
   )
 }
 
