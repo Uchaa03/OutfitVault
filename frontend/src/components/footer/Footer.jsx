f4922c89662eeca964e37529c0df3290801e4d6f
Llavesuke <llavesukeprogram@gmail.com>
2025-01-30 18:46:31 +0100

    Form responsive

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from '../button/button';
 
 const Footer = () => {
-    const navigate = useNavigate(); // React Router's navigation hook
+    const navigate = useNavigate();
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const handleClick = () => {
-      console.log("Navigating to the contact page...");
-      navigate("/contact"); // Redirect to the registration page
+        navigate("/contact");
     };
   
     return (
-      <section className='footer'>
-        <p className='footer__terms-and-conditions'>
-          Por OutfitVault ©2025 Adrián Ucha, Pablo Barrera, Maurice Darner está
-          autorizado bajo CC BY-NC-ND 4.0 licencia CC BY-NC-ND 4.0
-        </p>
-        <Button className='footer__button' onClick={handleClick}>
-          Contáctanos
-        </Button>
-      </section>
+        <section className='footer'>
+            <p className='footer__terms-and-conditions'>
+                {isMobile ? 'CC BY-NC-ND 4.0' : 
+                'Por OutfitVault ©2025 Adrián Ucha, Pablo Barrera, Maurice Darner está autorizado bajo CC BY-NC-ND 4.0 licencia CC BY-NC-ND 4.0'}
+            </p>
+            <Button className='footer__button' onClick={handleClick}>
+                Contáctanos
+            </Button>
+        </section>
     )
-  }
+}
 
 export default Footer
