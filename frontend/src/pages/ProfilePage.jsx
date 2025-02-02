baaf81f995d383cea2d56752a90c657375b497cd
Llavesuke <llavesukeprogram@gmail.com>
2025-02-02 18:20:27 +0100

    Profile page updated

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -47,28 +47,19 @@ const ProfilePage = () => {
         <main className="main__profile">
             <section className="profile__section">
                 <header className="section__header">
-                    <h1 className="header__title">Perfil de Usuario</h1>
+                    <h1 className="header__title">{isEditing?"Editar Usuario":"Perfil de Usuario"}</h1>
                 </header>
                 <section className="section__data">
-                    <article className="data__article">
-                        <p className="article__text">
-                            <span className="article__title">Usuario: </span>
-                            {userData.user.username}
-                        </p>
-                        <p className="article__text">
-                            <span className="article__title">Correo: </span>
-                            {userData.user.email}
-                        </p>
-                        <section className="article__buttons">
-                            <button className="buttons__button">Editar Usuario</button>
-                            <button className="buttons__button" onClick={() => logout()}>Cerrar Sesión</button>
-                        </section>
-                    </article>
-                    <img className="data__img" src="/assets/img/IconUser.svg" alt="Imagen de perfil de usuairo"/>
+                    {isEditing ? (
+                        <ProfileEditCard userData={userData} setEdit={setEdit} />
+                    ) : (
+                        <ProfileDisplayCard userData={userData} setEdit={setEdit} logout={logout} />
+                    )}
+                    <img className="data__img" src="/assets/img/IconUser.svg" alt="Imagen de perfil de usuario"/>
                 </section>
             </section>
         </main>
-    )
-}
+    );
+};
 
 export default ProfilePage
